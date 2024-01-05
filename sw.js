766957dd10bfd6b26e74faae9b33026273f54ea7
pghn <jwegoni@gmail.com>
2024-01-05 14:12:15 +0800

    update all

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704360096';
+const cacheName = 'chirpy-1704435123';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
